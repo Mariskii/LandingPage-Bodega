f347b2173ebef3aba50db9a11f05918b38760db0
Vicent Paya <vicentpayaginer@gmail.com>
2026-09-07 17:50:14 +0200

    Initial commit: install i18n and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://tubodega.com',
+  integrations: [tailwind(), sitemap()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en', 'fr', 'de'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  build: {
+    format: 'directory'
+  }
+});
