d19285a34fb13dfc36825beef4d3a7f7bebed17e
Vicent Paya <vicentpayaginer@gmail.com>
2026-09-09 13:56:07 +0200

    refactor: change index route

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tubodega.com',
+  srcDir: './src/pages',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
