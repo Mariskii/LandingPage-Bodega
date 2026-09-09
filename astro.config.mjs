45369b37292fc87126739e5403b66ddcc07142e4
Vicent Paya <vicentpayaginer@gmail.com>
2026-09-09 13:31:12 +0200

    feat: install tailwind and solve conflict with astro 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tubodega.com',
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'fr', 'de'],
